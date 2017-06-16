ad798e83f56ec4c15dbf3d016563ff7d9a8668a6
Taimoor Saeed <Admin@Nanos-Mac.local>
2017-06-16 19:13:26 +0500

    complete app

--- TodoUsingIFirebase/ViewController.swift
+++ TodoUsingIFirebase/ViewController.swift
@@ -8,18 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            self.tasks.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Edit Task", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addTextField(configurationHandler: {
+            (UITextField) in
+            UITextField.text = self.tasks[indexPath.row]
+        })
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default, handler: { (action) -> Void in
+            self.tasks[indexPath.row] = alert.textFields![0].text!
+            self.tableView.reloadData()
+        })
+        
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: { (action) -> Void in })
+        
+        alert.addAction(submitAction)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func AddTaskBtnPressed(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Add Task", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default, handler: { (action) -> Void in
+            let textField = alert.textFields![0]
+            self.tasks.append(textField.text!)
+            self.tableView.reloadData()
+        })
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: { (action) -> Void in })
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.keyboardAppearance = .dark
+            textField.keyboardType = .default
+            textField.autocorrectionType = .default
+            textField.placeholder = "Enter Task here"
+            textField.clearButtonMode = .whileEditing
+        }
+        
+        alert.addAction(submitAction)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
